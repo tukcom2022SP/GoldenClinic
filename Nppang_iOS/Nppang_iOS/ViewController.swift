25b66ec6787f45428d71aa531d37d5addb28d5d4
권태우 KwonTaewoo <kwontaewoo0901@gmail.com>
2022-07-28 14:54:11 +0900

    Add google login

--- Nppang_iOS/Nppang_iOS/ViewController.swift
+++ Nppang_iOS/Nppang_iOS/ViewController.swift
@@ -6,12 +6,15 @@
 //
 
 import UIKit
+import GoogleSignIn
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var googleSignIn: GIDSignInButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        GIDSignIn.sharedInstance()?.presentingViewController = self
     }
 
     
