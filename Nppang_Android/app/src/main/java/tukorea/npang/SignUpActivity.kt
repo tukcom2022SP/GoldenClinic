b8ac3dc904402ea1420b41ca82323021f6bb3467
gu <82009882+kugu98@users.noreply.github.com>
2022-08-01 18:56:18 +0900

    FireStore Connect

--- Nppang_Android/app/src/main/java/tukorea/npang/SignUpActivity.kt
+++ Nppang_Android/app/src/main/java/tukorea/npang/SignUpActivity.kt
@@ -5,11 +5,15 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_sign_up.*
 import tukorea.npang.databinding.ActivitySignUpBinding
 
 
 class SignUpActivity : Activity() {
+    //db선언
+    val db = Firebase.firestore
     private lateinit var binding: ActivitySignUpBinding
 
     //firebase Auth
@@ -18,12 +22,33 @@ class SignUpActivity : Activity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         firebaseAuth = FirebaseAuth.getInstance()
 
         //회원가입시 파이어베이스 정보 등록
         binding.btnSignUp.setOnClickListener {
-            createEmail(et_sign_up_email.text.toString(), et_sign_up_passwd.text.toString())
+            createEmail(
+                et_sign_up_email.text.toString().trim(),
+                et_sign_up_passwd.text.toString().trim()
+            )
+            val userBankAccount = binding.etSignUpEmail.text
+            val userEmail = binding.etUserName.text
+            val userName = binding.etAccount.text
+            val userPhoneNumber = binding.etPhoneNumber.text
+            var UserInfoMation = hashMapOf(
+                "userBankAccount" to userBankAccount.toString().trim(),
+                "userEmail" to userEmail.toString().trim(),
+                "userName" to userName.toString().trim(),
+                "userPhoneNumber" to userPhoneNumber.toString().trim()
+            )
+
+
+            db.collection("UserData").add(UserInfoMation)
+                .addOnSuccessListener { documentReference ->
+                    Log.d("userdata", "DocumentSnapshot added with ID: ${documentReference.id}")
+                }
+                .addOnFailureListener { e ->
+                    Log.w("userdata", "Error adding document", e)
+                }
             Log.d("btn", "onCreate: ${et_sign_up_email.text}")
         }
     }
