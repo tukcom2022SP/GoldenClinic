12b03c18583f5381c1dc6e2c8662cb3d760240dc
YUBIN CHOE <cybin050300@naver.com>
2022-08-01 15:33:56 +0900

    commit

--- Nppang_Android/app/src/main/java/tukorea/npang/CategoryDetailsActivity.kt
+++ Nppang_Android/app/src/main/java/tukorea/npang/CategoryDetailsActivity.kt
@@ -1,7 +1,23 @@
 package tukorea.npang
 
 import android.app.Activity
+import android.os.Bundle
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_category.*
+import tukorea.npang.databinding.ActivityCategoryDetailsBinding
 
-class CategoryDetailsActivity : Activity() {
+class CategoryDetailsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCategoryDetailsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCategoryDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        chickenimage.setOnClickListener {
+            binding.categorytitle.text="카테고리-치킨"
+        }
+    }
 
 }
