955777ac74838874fc0044741f3ae7594ad13666
gu <82009882+kugu98@users.noreply.github.com>
2022-08-02 22:06:54 +0900

    실시간게시물
    실시간게시물 파이어베이스연동해서 읽어오기

--- Nppang_Android/app/src/main/java/tukorea/npang/CategoryDetailsActivity.kt
+++ Nppang_Android/app/src/main/java/tukorea/npang/CategoryDetailsActivity.kt
@@ -1,24 +1,31 @@
 package tukorea.npang
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import tukorea.npang.databinding.ActivityCategoryDetailsBinding
 
-class CategoryDetailsActivity : AppCompatActivity() {
+class CategoryDetailsActivity() : Activity() {
+    //binding
     private lateinit var binding: ActivityCategoryDetailsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCategoryDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
         //카테고리 Name설정
         val firstintent = intent
         binding.tvCategorytitle.text = firstintent.getStringExtra("카테고리")
 
-        binding.btnWritePost.setOnClickListener{
+        binding.btnWritePost.setOnClickListener {
             val intent = Intent(this, PostingActivity::class.java)
             startActivity(intent)
         }
+
     }
+
+
 }
+
