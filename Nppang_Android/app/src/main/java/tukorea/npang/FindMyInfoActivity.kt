753af89d19d5ebceec5749512e0d08f44f3f6190
YUBIN CHOE <cybin050300@naver.com>
2022-08-03 21:40:19 +0900

    reset passwd

--- Nppang_Android/app/src/main/java/tukorea/npang/FindMyInfoActivity.kt
+++ Nppang_Android/app/src/main/java/tukorea/npang/FindMyInfoActivity.kt
@@ -2,10 +2,30 @@ package tukorea.npang
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import tukorea.npang.databinding.ActivityFindMyInfoBinding
 
 class FindMyInfoActivity : Activity() {
+
+    //firebase Auth
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    private lateinit var binding: ActivityFindMyInfoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_find_my_info)
+        binding = ActivityFindMyInfoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnResetPasswd.setOnClickListener {
+            findPassword()
+        }
+    }
+
+    fun findPassword() {
+        FirebaseAuth.getInstance().sendPasswordResetEmail(binding.etFindEmail.text.toString().trim())
+            .addOnSuccessListener {
+                Toast.makeText(this, "비밀번호 재설정 이메일을 전송했습니다.", Toast.LENGTH_SHORT).show()
+            }
     }
 }
