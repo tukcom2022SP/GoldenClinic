a8a446b2049424b4112e71e74adf85ed4e2293bf
권태우 KwonTaewoo <kwontaewoo0901@gmail.com>
2022-08-08 15:25:44 +0900

    Create MyPageVC.swift

---
+++ Nppang_iOS/Nppang_iOS/MyPageVC.swift
@@ -0,0 +1,17 @@
+//
+//  MyPageVC.swift
+//  Nppang_iOS
+//
+//  Created by 권태우 on 2022/08/08.
+//
+
+import UIKit
+import Firebase
+
+class MaPageVC: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
